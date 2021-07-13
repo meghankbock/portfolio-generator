fc214ed13ee8f22d403ecb7eb08f5467f29a0609
Meghan Bock <meghan.bock@gmail.com>
2021-07-13 00:29:32 -0500

    create page-template module and install in app.js file

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name,github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Checkout index.html to see the output!');
+});
+
+/*const printProfileData = profileDataArr => {
     profileDataArr.forEach((profileItem) => console.log(profileItem));
 };
-
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);*/
