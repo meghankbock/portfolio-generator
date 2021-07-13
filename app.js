e25979c6787d8f8e54098fab28dea0098a7ae8d6
Meghan Bock <meghan.bock@gmail.com>
2021-07-13 01:07:46 -0500

    import inquirer npm

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+/*const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+const pageHTML = generatePage(name,github);
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('index.html', generatePage(name,github), err => {
+fs.writeFile('./index.html', pageHTML, err => {
     if (err) throw err;
 
     console.log('Portfolio complete! Checkout index.html to see the output!');
-});
+});*/
 
 /*const printProfileData = profileDataArr => {
     profileDataArr.forEach((profileItem) => console.log(profileItem));
